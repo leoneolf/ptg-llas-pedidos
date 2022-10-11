e1f5775fe4223a2f2482c1621a88a55c13db7812
Leone Oliveira <oliveiraleone75@gmail.com>
2022-10-10 22:12:37 -0300

    Add botão do Google, rs

--- app/src/main/java/com/ptg/llas_pedidos/MainActivity.kt
+++ app/src/main/java/com/ptg/llas_pedidos/MainActivity.kt
@@ -1,17 +1,55 @@
 package com.ptg.llas_pedidos
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.ptg.llas_pedidos.ui.theme.LlasPedidosTheme
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LlasPedidosTheme {
-                MainScreen()
+//                MainScreen()
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    GoogleButton(
+                        text = "Logar com o Google",
+                        loadingText = "Criando a Conta...",
+                        onClicked = {
+                            Log.d("googleButton", "Clicked!")
+                        }
+                    )
+                }
             }
+        }
+    }
+}
+
+@ExperimentalMaterialApi
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LlasPedidosTheme {
+        Column (
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            GoogleButton {}
         }
     }
 }
